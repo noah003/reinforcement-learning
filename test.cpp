78fb3bcaa6c4626a9d3de6fd8f73cba80c791785
noah guo <bit_gyn@163.com>
2018-03-20 14:51:55 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,2 +1,14 @@
 #define TIMER_START boost::timer t_##__func__;
 #define TIMER_END std::cout << "[" << #__func__ << "]" << "cost time: " << t_##__func__.elapsed() << std::endl;
+
+void FunctionA() {
+  TIMER_START(FunctionA)
+  SomeFunction();
+  TIMER_END(FunctionA)
+}
+
+void FunctionA() {
+  TIMER_START
+  SomeFunction();
+  TIMER_END
+}
